122af1bf0aa6d2801c41a1051028bd08a6b3ecdf
Majjcom <majjcom@126.com>
2025-07-24 23:22:06 +0800

    fix file size issue

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     const int font_id = QFontDatabase::addApplicationFont("://fonts/MapleMono-SC-NF-Regular.ttf");
-    QFontDatabase::addApplicationFont("://fonts/MapleMono-SC-NF-LightItalic.ttf");
     QString font_name = QFontDatabase::applicationFontFamilies(font_id)[0];
 
     QFont font(font_name);
